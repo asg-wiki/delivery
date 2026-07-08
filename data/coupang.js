775b91ecca778c5f6138bc56dcf196652c2afe6b
asg-wiki <asg.of.003@gmail.com>
2026-07-08 21:37:31 +0900

    관리자: 리뷰이관 후 신규 매장 노출 기준 자주 찾음 지정

--- data/coupang.js
+++ data/coupang.js
@@ -73,7 +73,8 @@ window.WIKI_DATA.coupang = [
       "신규매장"
     ],
     "updated": "2026-07-07",
-    "last_verified": "2026-07-08"
+    "last_verified": "2026-07-08",
+    "favorite": true
   },
   {
     "id": "coupang-happycall-process",
